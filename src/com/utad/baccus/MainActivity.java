0b675511a2e3f61e7af857a135c5c4fde15ddf29
Pablo Parejo <parejo.p@gmail.com>
2014-11-07 20:17:53 +0100

    Added WineModel to the project

--- src/com/utad/baccus/MainActivity.java
+++ src/com/utad/baccus/MainActivity.java
@@ -5,21 +5,32 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.utad.baccus.models.WineModel;
+
 
 public class MainActivity extends ActionBarActivity {
+	
+	private WineModel vegaval = null;
+	private WineModel bembibre = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        vegaval = new WineModel(R.drawable.vegaval, 4, "Vegaval", "Tinto", "http://www.vegaval.com/es/", "Miguel Calatayud)");
+        vegaval.addGrape("Mencía");
+        
+        bembibre = new WineModel(R.drawable.bembibre, 5, "Bembibre", "Tinto", "http://www.dominiodetares.com/index.php/es/", "Dominio de Tares");
+        bembibre.addGrape("Mencía");
     }
     
     public void changeImage(View v){
     	ImageView wineImage = (ImageView) findViewById(R.id.wineImage);
     	if (v.getId() == R.id.btn_change_bembibre) {
-    		wineImage.setImageResource(R.drawable.bembibre);
+    		wineImage.setImageResource(bembibre.getImage());
 		}else if (v.getId() == R.id.btn_change_vegaval) {
-			wineImage.setImageResource(R.drawable.vegaval);
+			wineImage.setImageResource(vegaval.getImage());
 		}
     }
 }
---
+++ src/com/utad/baccus/models/WineModel.java
@@ -0,0 +1,87 @@
+package com.utad.baccus.models;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class WineModel {
+	
+	private int mImage;
+	private int mRating;
+	private List<String> mGrapes;
+	private String mName;
+	private String mType;
+	private String mURL;
+	private String mWinehouse;
+	
+	public WineModel(int image, int rating, String name,
+			String type, String uRL, String winehouse) {
+		super();
+		mImage = image;
+		mRating = rating;
+		mGrapes = new LinkedList<String>();
+		mName = name;
+		mType = type;
+		mURL = uRL;
+		mWinehouse = winehouse;
+	}
+	
+	public void addGrape(String grape){
+		mGrapes.add(grape);
+	}
+
+	public int getImage() {
+		return mImage;
+	}
+
+	public void setImage(int image) {
+		mImage = image;
+	}
+
+	public int getRating() {
+		return mRating;
+	}
+
+	public void setRating(int rating) {
+		mRating = rating;
+	}
+
+	public List<String> getGrapes() {
+		return mGrapes;
+	}
+
+	public String getName() {
+		return mName;
+	}
+
+	public void setName(String name) {
+		mName = name;
+	}
+
+	public String getType() {
+		return mType;
+	}
+
+	public void setType(String type) {
+		mType = type;
+	}
+
+	public String getURL() {
+		return mURL;
+	}
+
+	public void setURL(String uRL) {
+		mURL = uRL;
+	}
+
+	public String getWinehouse() {
+		return mWinehouse;
+	}
+
+	public void setWinehouse(String winehouse) {
+		mWinehouse = winehouse;
+	}
+	
+	
+	
+
+}
